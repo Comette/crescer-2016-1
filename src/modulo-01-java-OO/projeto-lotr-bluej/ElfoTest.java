fa761ae66050188b359fdfea9b816d9fa7d24f36
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-06 17:04:44 -0300

    refactor(ElfoTest): melhorar retorno dos testes, utilizando assertTrue();

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoTest.java
@@ -9,13 +9,13 @@ public class ElfoTest
     public void criaElfoSohComNome(){
         Elfo elfo = new Elfo("Legolas");
         boolean resultado = (elfo.getNome() == "Legolas" && elfo.getFlechas() == 42);
-        assertEquals(true, resultado);
+        assertTrue(resultado);
     }
     @Test
     public void criaElfoComNomeEFlechas(){
         Elfo elfo = new Elfo("Legolas", 20);
         boolean resultado = (elfo.getNome() == "Legolas" && elfo.getFlechas() == 20);
-        assertEquals(true, resultado);
+        assertTrue(resultado);
     }
     @Test
     public void testaAtirarFlecha(){
@@ -23,14 +23,12 @@ public class ElfoTest
         Dwarf anao = new Dwarf("Sofredor");
         elfo.atirarFlecha(anao);
         boolean resultado = (elfo.getFlechas() == 41 && elfo.getExperiencia() == 1 && anao.getVida() == 100);
-        assertEquals(true, resultado);
+        assertTrue(resultado);
     }
     @Test
     public void testaToString(){
         Elfo elfo = new Elfo("Legolas");
-        String correta = "Legolas possui 42 flechas e 0 niveis de experiencia.";
-        String resultado = elfo.toString();        
-        assertEquals(true, correta.equals(resultado));
-        
+        String correta = "Legolas possui 42 flechas e 0 niveis de experiencia.";                
+        assertTrue(correta.equals(elfo.toString()));        
     }
 }
