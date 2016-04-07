dd300b50885e6740c46396f0f9b67ad4d4cae20a
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-07 15:57:19 -0300

    feat(Condicao, Status): Implementação de Enums utilizados por Dwarves; feat(Dwarf): implementação de atributo status e metodo getStatus(). Implementação de alteração de status conforme leva flechas e fica com vida 0; test(DwarfTest): implementação de test da alteração de status;

--- src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/Dwarf.java
@@ -2,13 +2,16 @@ public class Dwarf
 {
     private int vida = 110;
     private String nome;
+    private Status status = Status.VIVO;
+    
     public Dwarf(String nome)
     {        
         this.nome = nome;
     }
     public void levaFlechada(){
-        if (vida >= 10){
-            vida -= 10;
+        vida -= 10;
+      if(vida == 0){
+            status = Status.MORTO;
         }
     }
     public void setNome(String nome){
@@ -19,5 +22,8 @@ public class Dwarf
     }
     public int getVida(){
         return vida;
+    }
+    public Status getStatus(){
+        return status;
     }
 }
--- src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/DwarfTest.java
@@ -18,4 +18,18 @@ public class DwarfTest
         anao.levaFlechada();        
         assertEquals(true, anao.getVida() == 100);
     }    
+    @Test
+    public void testaDwarfNasceVivo(){
+        Dwarf anao = new Dwarf("Nome");                
+        assertEquals(Status.VIVO, anao.getStatus());
+    } 
+    @Test
+    public void mataDwarfCom11Flechadas(){
+        Dwarf dwarf = new Dwarf("Gimli");
+        Elfo elfo = new Elfo("Legolas");
+        for(int i=0; i<11; i++){
+            elfo.atirarFlecha(dwarf);
+        }
+        assertEquals(Status.MORTO, dwarf.getStatus());
+    }
 }
