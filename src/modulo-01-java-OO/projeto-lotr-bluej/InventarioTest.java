f94b4022677770037989e4903ba539a2db644301
Victor Ribeiro <Victor Ribeiro>
2016-04-11 09:35:11 -0300

    test(InventarioTest): implementações para testar os metodos getDescricoesItens, itemMaisPopular e ordenarItens;

--- src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/InventarioTest.java
@@ -36,4 +36,52 @@ public class InventarioTest
         
         assertEquals(1, bolsa.getQuantidadeItens());
     }
+    @Test
+    public void testaAdiciona3ItensEPegaDescricao(){
+        Inventario bolsa = new Inventario();
+        String esperado = "pedra, corda, pocao";
+        Item i1 = new Item(10, "pedra");
+        Item i2 = new Item(15, "corda");
+        Item i3 = new Item(16, "pocao");
+        
+        bolsa.adicionaItem(i1);
+        bolsa.adicionaItem(i2);
+        bolsa.adicionaItem(i3);
+               
+        assertTrue(esperado.equals(bolsa.getDescricoesItens()));
+    }
+    @Test
+    public void testaItemMaisPopular(){
+        Inventario bolsa = new Inventario();
+        Item i1 = new Item(10, "pedra");
+        Item i2 = new Item(12, "corda");
+        Item i3 = new Item(18, "pocao");
+        
+        bolsa.adicionaItem(i1);
+        bolsa.adicionaItem(i2);
+        bolsa.adicionaItem(i3);
+        
+        assertEquals(i3, bolsa.itemMaisPopular());
+    }
+    @Test
+    public void testaOrdenacaocincoItens(){
+        Inventario bolsa = new Inventario();
+        Item i1 = new Item(18, "pedra");
+        Item i2 = new Item(10, "corda");
+        Item i3 = new Item(15, "pocao");
+        Item i4 = new Item(12, "machado");
+        Item i5 = new Item(8, "picareta");
+        
+        bolsa.adicionaItem(i1);
+        bolsa.adicionaItem(i2);
+        bolsa.adicionaItem(i3);
+        bolsa.adicionaItem(i4);
+        bolsa.adicionaItem(i5);
+        
+        String esperado = "picareta, corda, machado, pocao, pedra";
+        
+        bolsa.ordenarItens();
+        
+        assertTrue(esperado.equals(bolsa.getDescricoesItens()));
+    }
 }
