c0726899d01b4165132a6bba0fd445ffdcc53c1d
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-11 13:46:52 -0300

    refactor(HobbitContador): metodo calcularDiferenca() otimizado;

--- src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/HobbitContador.java
@@ -4,25 +4,17 @@ public class HobbitContador
     public HobbitContador(){
         
     }
-    public int calcularDiferenca(ArrayList<ArrayList<Integer>> array){
-        int a = 0;
-        int b = 0;
+    public int calcularDiferenca(ArrayList<ArrayList<Integer>> array){        
         int soma = 0;
         ArrayList<Integer> arrayMMC = new ArrayList<>();
         ArrayList<Integer> arrayProdutos = new ArrayList<>();
         ArrayList<Integer> arrayDiferencas = new ArrayList<>();
-        for(int i = 0; i< array.size(); i++){
-            a = array.get(i).get(0);
-            b = array.get(i).get(1);
-            arrayMMC.add(calculaMMC(a, b));
-            arrayProdutos.add(a*b);            
-        }
-        for(int i = 0; i < array.size(); i++){
+        for(int i = 0; i< array.size(); i++){            
+            arrayMMC.add(calculaMMC(array.get(i).get(0), array.get(i).get(1)));
+            arrayProdutos.add(array.get(i).get(0)*array.get(i).get(1));   
             arrayDiferencas.add(arrayProdutos.get(i) - arrayMMC.get(i));
-        }
-        for(int i = 0; i < array.size(); i++){
             soma += arrayDiferencas.get(i);
-        }
+        }        
         return soma;        
     }
     private int calculaMMC(int a, int b){
