72f88169288acebec84701d02cf36a6fa21a349f
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-13 17:33:57 -0300

    (EM ANDAMENTO)

--- src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ExercitoDeElfos.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class ExercitoDeElfos
 {
-    HashMap<String, Elfo> exercitoDeElfos = new HashMap<>();    
+    HashMap<String, Elfo> exercitoDeElfos = new HashMap<>();  
+    HashMap<Status, ArrayList> exercitoDeElfosAgrupados = new HashMap<>();
+    ArrayList<Elfo> elfosVivos = new ArrayList<>();
     public void alistaElfo(Elfo elfo){
         if(elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno){
             String key = elfo.getNome();
@@ -11,4 +13,14 @@ public class ExercitoDeElfos
     public Elfo buscaElfoPeloNome(String nome){
         return exercitoDeElfos.get(nome);    
     }
+    /*public void agrupaElfosPorStatus(){
+        for(Elfo elfo : exercitoDeElfos.valueSet()){
+            if(elfo.getStatus == Status.VIVO){    
+                elfosVivos.add(elfo);                
+            }else if(elfo.getStatus == Status.MORTO){
+                elfosMortos
+        }
+        exercitoDeElffosAgrupado.put(Status.VIVO, elfosVivos);
+    }*/
+        
 }
