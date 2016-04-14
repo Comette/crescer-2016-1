30148f3a3982f3350f1aa99329257db9328091e5
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-14 14:43:16 -0300

    test(IrishDwarfTest): adicionado novo teste para tentaSorte() e melhorada a organização do código;

--- src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarfTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/IrishDwarfTest.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -7,15 +5,30 @@ import org.junit.Test;
 
 public class IrishDwarfTest
 {
+    //TESTES
     @Test
-    public void testaTentarSorteComNumeroCorreto(){
+    public void tentarSorteComNumeroCorreto(){
+        //Arrange
         IrishDwarf irish = new IrishDwarf("Trevoso", new DataTerceiraEra(1,1,2016));
         for(int i=0; i<3; i++){
             irish.levaFlechada();
         }
         Item item = new Item(4, "pedra");
         irish.ganhaItem(item);
-        irish.tentarSorte();        
+        //Act
+        irish.tentarSorte();  
+        //Assert
         assertEquals(10004, irish.getInventario().getItemNoInventario(0).getQuantidade());
-    } 
+    }
+    @Test
+    public void tentarSorteComNumeroErrado(){
+        //Arrange  
+        IrishDwarf irish = new IrishDwarf("Trevoso", new DataTerceiraEra(1,1,2016));        
+        Item item = new Item(4, "pedra");
+        irish.ganhaItem(item);
+        //Act
+        irish.tentarSorte();
+        //Assert
+        assertEquals(4, irish.getInventario().getItemNoInventario(0).getQuantidade());
+    }
 }
