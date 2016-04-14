88d9124afac9ed95183ab8552d8cf57039daecbe
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-04-14 14:58:17 -0300

    Style(ElfoNoturnoTest): melhorada a organização do código;

--- src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
+++ src/modulo-01-java-OO/projeto-lotr-bluej/ElfoNoturnoTest.java
@@ -5,36 +5,49 @@ import org.junit.Test;
 
 public class ElfoNoturnoTest
 {
+    //TESTES atirarFlecha()
     @Test
-    public void testaElfoGanhaTriploExperienciaAoAtirarFlecha(){
+    public void elfoGanhaTriploExperienciaAoAtirarFlecha(){
+        //Arrange
         Elfo elfo = new ElfoNoturno("Legolas da Nigth");
         Dwarf anao = new IrishDwarf("Feito pra Morrer");
+        //Act
         elfo.atirarFlecha(anao);
+        //Assert
         assertEquals(3, elfo.getExperiencia());
     }
     @Test
-    public void testaElfoPerde5PorCentoDeVidaAoAtirarFlecha(){
+    public void elfoPerde5PorCentoDeVidaAoAtirarFlecha(){
+        //Arrange
         Elfo elfo = new ElfoNoturno("Legolas da Nigth");
         Dwarf anao = new IrishDwarf("Feito pra Morrer");
+        //Act
         elfo.atirarFlecha(anao);        
+        //Assert
         assertEquals(95, elfo.getVida(), 0.0001);
     }
     @Test
-    public void testaElfoMorreAoAtirar150Vezes(){
+    public void elfoMorreAoAtirar150Vezes(){
+        //Arrange
         Elfo elfo = new ElfoNoturno("Legolas da Nigth", 150);
         Dwarf anao = new IrishDwarf("Feito pra Morrer");
+        //Act
         for (int i = 0; i< 150; i++){
             elfo.atirarFlecha(anao);  
         }
+        //Assert
         assertEquals(Status.MORTO, elfo.getStatus());
     }
     @Test
-    public void testaElfoMorreAoAtirar89Vezes(){
+    public void elfoMorreAoAtirar89Vezes(){
+        //Arrange
         Elfo elfo = new ElfoNoturno("Legolas da Nigth", 150);
         Dwarf anao = new IrishDwarf("Feito pra Morrer");
+        //Act
         for (int i = 0; i< 89; i++){
             elfo.atirarFlecha(anao);  
         }
+        //Assert
         assertEquals(Status.VIVO, elfo.getStatus());
     }
 }
