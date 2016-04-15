817bdac00ef0b8a8739847ebb0662d41e1b22203
Victor Ribeiro <Victor Ribeiro>
2016-04-15 11:47:48 -0300

    feat(EstrategiaNoturnosPorUltimo): implementada nova estategia e sua classe de teste;

---
+++ src/modulo-01-java-OO/projeto-lotr-bluej/EstrategiaNoturnosPorUltimo.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class EstrategiaNoturnosPorUltimo implements Estrategia
+{
+    private ExercitoDeElfos exercito = new ExercitoDeElfos();
+    private Map<String, Elfo> ordemUltimoAtaque = new HashMap<String, Elfo>();    
+    public void atacaHordaDwarves(ArrayList<Elfo> elfosQueAtacam, ArrayList<Dwarf> hordaDwarves){ 
+        elfosQueAtacam = organizaLista(elfosQueAtacam);
+        for(Elfo elfo: elfosQueAtacam){            
+            for(Dwarf dwarf : hordaDwarves){
+                elfo.atirarFlecha(dwarf);
+                ordemUltimoAtaque.put(elfo.getNome(), elfo);
+            }
+        }
+    }
+    public ArrayList<Elfo> getOrdemUltimoAtaque(){
+        ArrayList<Elfo> retorno = new ArrayList<Elfo>();
+        for(Elfo elfo : ordemUltimoAtaque.values()){
+            retorno.add(elfo);
+        }
+        return retorno;
+    }
+    private ArrayList<Elfo> organizaLista(ArrayList<Elfo> lista){
+        ArrayList<Elfo> listaOrganizada = new ArrayList<Elfo>();
+        for(Elfo elfo : lista){
+            if (elfo instanceof ElfoVerde){
+                listaOrganizada.add(0, elfo);
+            }else{
+                listaOrganizada.add(elfo);
+            }
+        }
+        return listaOrganizada;
+    }
+}
