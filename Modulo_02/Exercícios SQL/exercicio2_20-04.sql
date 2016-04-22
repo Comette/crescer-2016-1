2656c2e67d8d2598549f505045d86e3be868cd97
Victor Ribeiro <Victor Ribeiro>
2016-04-22 09:45:52 -0300

    feat(Exercicio02): exercicios completados!

--- Modulo_02/Exercícios SQL/exercicio2_20-04.sql
+++ Modulo_02/Exercícios SQL/exercicio2_20-04.sql
@@ -1,4 +1,4 @@
-Select * from Associado
+Select * from Cidade
 
 ---Questao 01
 select left(Nome, (CharIndex(' ', Nome) -1)) from Associado
@@ -14,4 +14,57 @@ select NomeEmpregado, DATEDIFF(MONTH, DataAdmissao, CONVERT(datetime, '31/12/200
 ---Questao 04
 select top 1 Cargo, COUNT(IDEmpregado) from Empregado group by Cargo order by count(IDEmpregado) desc
 
+---Questao 05
+/*DON'T EXISTS*/
+
+---Questão 06
+select Nome, 
+	convert(date,DATEADD(year, 50, DataNascimento)) as data50Anos, 
+	DATENAME(dw, DATEADD(year, 50, DataNascimento)) as DiaDaSemana 
+	From Associado
+
+---Questao 07
+select UF, count(IDCidade) as QuantidadeCidades from Cidade group by UF 
+
+---Questao 08
+select Nome, UF, count(*) as Repeticoes from Cidade group by Nome, UF having count(*) > 1
+
+---Questao 09
+select top 1 (IDCidade+1) as ProximoID from Cidade order by IDCidade desc
  
+ ---Questao 10
+ truncate table cidadeaux
+ create table cidadeaux2 (IDcidade int identity not null, Nome varchar(90) not null, UF varchar(2) not null)
+ insert into CidadeAux2 select distinct Nome, UF from Cidade
+ insert into CidadeAux select IDCidade, Nome, UF from CidadeAux2
+ drop table cidadeaux2
+
+ ---Questao 11
+ begin transaction
+ update Cidade set Nome = '*' + Nome
+	 where Nome in (select Nome from Cidade group by Nome, UF having count(*) > 1)
+ commit
+
+ ---Questao 12
+ select Nome, case Sexo
+	when 'F' then 'FEMININO'
+	when 'M' then 'MASCULINO'
+	else 'Nao Informado'
+	end as Sexo from Associado
+
+---Questao 13
+select NomeEmpregado, Salario, Case
+	when salario <= 1164.00 then '0%'
+	when salario between 1164.01 and 2326.00 then '15%'
+	when salario > 2326.00 then '27,5%'
+	end as PercentualImpostoRenda from Empregado
+
+---Questao 14
+begin transaction
+delete from Cidade where ((charindex('*', Nome)=1) and IDCidade in (select MAX(IDCidade) from Cidade group by nome, UF))
+commit
+
+--Questao 15
+begin transaction
+alter table cidade add constraint Cidade_UF unique (Nome, UF)
+commit
