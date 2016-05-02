c6175f8608c06fe99339f00623555a1e989c8d51
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-05-02 17:31:03 -0300

    feat(tema-dia01): EM ANDAMENTO

---
+++ Modulo_04/Dia01_Tema/lista-js-basico.js
@@ -0,0 +1,11 @@
+'use strict'
+//----------01
+var daisyGame = function(numPetalas){
+  return numPetalas%2!==0 ? 'Love me' : 'Love me not';
+}
+//----------02
+var maiorTexto = funcion(array){
+  if(array.construtor === Array){
+    array.foreach()
+  }
+}
