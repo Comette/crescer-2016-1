cdd302d9c68a37d99ece68f04c385b8d3d35ae0c
Victor Ribeiro <Victor Ribeiro>
2016-05-07 17:22:23 -0300

    feat(Tema-Dia01) Nivel 2

---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01/Upgrade/CanhaoPlasma.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Dia01.Upgrade
+{
+    public class CanhaoPlasma : IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get { return 2; }
+        }
+
+        public int BonusDefesa
+        {
+            get { return 0; }
+        }
+    }
+}
---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01/Upgrade/EscudoEnergia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Dia01.Upgrade
+{
+    public class EscudoEnergia:IUpgrade
+    {
+        public int BonusAtaque
+        {
+            get { return 0; }
+        }
+
+        public int BonusDefesa
+        {
+            get { return 2; }
+        }
+    }
+}
