365cc2d117f2d0bc613eccb9d4ca4e2e8b52714b
Victor Ribeiro <Victor Ribeiro>
2016-05-07 17:22:50 -0300

    feat(Tema-Dia01): Nivel 03

---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01-Tests/RushTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema_Dia01.Robos;
+using Tema_Dia01.Upgrade;
+
+namespace Tema_Dia01_Tests
+{
+    [TestClass]
+    class RushTest
+    {
+        [TestMethod]
+        public void RushNasceCom4Ataque3Vida()
+        {
+            var rush = new Rush();
+            var inutil = new BOT();
+            rush.Atacar(inutil);
+            inutil.Atacar(rush);
+            Assert.AreEqual(98, rush.Vida);
+            Assert.AreEqual(96, inutil.Vida);
+        }
+
+        [TestMethod]
+        public void RushGanhaMais2AtaqueComUpgradeCanhaoPlasma()
+        {
+            var rush = new Rush();
+            var inutil = new BOT();
+            var upCanhao = new CanhaoPlasma();
+            rush.EquipaUpgrade(upCanhao);
+            rush.Atacar(inutil);
+            Assert.AreEqual(94, inutil.Vida);
+        }
+
+        [TestMethod]
+        public void RushAdicionaSeuAtaqueMaisUpgradesQuandoUsadoComoUpgrade()
+        {
+            var rush = new Rush();
+            var megaman = new Megaman();
+            var inutil = new BOT();
+            var upCanhao = new CanhaoPlasma();
+            rush.EquipaUpgrade(upCanhao);
+            megaman.EquipaUpgrade(rush);
+            megaman.Atacar(inutil);
+            Assert.AreEqual(88, inutil.Vida);
+        }
+
+        [TestMethod]
+        public void RushAdicionaSuaDefesaQuandoUsadoComoUpgrade()
+        {
+            var rush = new Rush();
+            var megaman = new Megaman();
+            var inutil = new BOT();
+            megaman.EquipaUpgrade(rush);
+            inutil.Atacar(megaman);
+            Assert.AreEqual(97, megaman.Vida);
+        }
+    }
+}
---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01/Robos/Rush.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema_Dia01.Upgrade;
+
+namespace Tema_Dia01.Robos
+{
+    public class Rush : Robo, IUpgrade
+    {
+
+        public int BonusAtaque
+        {
+            get { return this.CalculaForcaAtaque(); }
+        }
+
+        public int BonusDefesa
+        {
+            get { return this.Defesa; }
+        }
+
+        public Rush()
+            : base()
+        {
+            this.Ataque = 4;
+            this.Defesa = 3;
+            this.Upgrades = new IUpgrade[2];
+        }
+    }
+}
