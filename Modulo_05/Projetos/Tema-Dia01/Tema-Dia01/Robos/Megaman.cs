5ede1d42acdd1d039b4f8d1a221d8dcf68d02908
Victor Ribeiro <Victor Ribeiro>
2016-05-07 17:20:40 -0300

    feat(Tema-Dia01): Nivel 01

---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01/Robos/Megaman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Dia01.Robos
+{
+    public class Megaman : Robo
+    {
+        public Megaman()
+            : base()
+        {
+            Ataque = 6;
+        }
+
+        public override void Atacar(Robo oponente)
+        {
+            int forca = this.CalculaForcaAtaque();
+            if (this.Vida <= 30)
+            {
+                oponente.RecebeAtaque(forca + 3);
+            }
+            else
+            {
+                oponente.RecebeAtaque(forca);
+            }
+        }
+    }
+}
---
+++ Modulo_05/Projetos/Tema-Dia01/Tema-Dia01/Robos/Robo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema_Dia01.Upgrade;
+
+namespace Tema_Dia01.Robos
+{
+    public class Robo
+    {
+        public int Vida { get; protected set; }
+        protected int Ataque { get; set; }
+        protected int Defesa { get; set; }
+
+        protected IUpgrade[] Upgrades { get; set; }
+
+        public Robo()
+        {
+            Ataque = 5;
+            Vida = 100;
+            Upgrades = new IUpgrade[3];
+        }
+
+        public virtual void Atacar(Robo oponente)
+        {
+            oponente.RecebeAtaque(this.CalculaForcaAtaque());
+        }
+
+        public virtual void RecebeAtaque(int forca)
+        {
+            int def = this.CalculaPoderDefesa();
+            if (this.Vida - (forca - def) < 0)
+            {
+                this.Vida = 0;
+            }
+            else
+            {
+                this.Vida -= forca - def;
+            }
+        }
+
+        public void EquipaUpgrade(IUpgrade upgrade)
+        {
+            for (int i = 0; i < Upgrades.Length; i++)
+            {
+                if (Upgrades[i] == null)
+                {
+                    Upgrades[i] = upgrade;
+                    break;
+                }
+            }
+        }
+
+        protected int CalculaForcaAtaque()
+        {
+            int forca = this.Ataque;
+            for (int i = 0; i < Upgrades.Length; i++)
+            {
+                if (Upgrades[i] != null)
+                {
+                    forca += Upgrades[i].BonusAtaque;
+                }                
+            }
+            return forca;
+        }
+
+        protected int CalculaPoderDefesa()
+        {
+            int def = this.Defesa;
+            for (int i = 0; i < Upgrades.Length; i++)
+            {
+                if (Upgrades[i] != null)
+                {
+                    def += Upgrades[i].BonusDefesa;
+                }                
+            }
+            return def;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + this.GetType().Name + ", Vida: " + this.Vida + ", Ataque: " + this.Ataque + "Defesa: " + this.Defesa;
+        }
+    }
+}
