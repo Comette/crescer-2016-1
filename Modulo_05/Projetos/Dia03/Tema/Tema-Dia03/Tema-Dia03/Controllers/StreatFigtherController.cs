b9f88bdf47a64544720c71e4e81d95e4a1a7996e
Victor Ribeiro <Victor Ribeiro>
2016-05-10 20:59:44 -0300

    feat(Tema-dia03): Exercicio pronto

---
+++ Modulo_05/Projetos/Dia03/Tema/Tema-Dia03/Tema-Dia03/Controllers/StreatFigtherController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tema_Dia03.Models;
+
+namespace Tema_Dia03.Controllers
+{
+    public class StreatFigtherController : Controller
+    {
+        //
+        // GET: /StreatFigther/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult SobreMim()
+        {
+            SobreMimModel modelo = new SobreMimModel("Victor", 22, 1.82, 76);
+            return View(modelo);
+        }
+	}
+}
