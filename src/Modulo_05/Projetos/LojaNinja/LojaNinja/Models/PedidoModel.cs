e7ac863e9b4a2e7317b82435638cf242a215ce76
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-05-12 17:41:31 -0300

    feat(LojaNinja): EM ANDAMENTO

---
+++ src/Modulo_05/Projetos/LojaNinja/LojaNinja/Models/PedidoModel.cs
@@ -0,0 +1,35 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaNinja.MVC.Models
+{
+    public class PedidoModel
+    {
+        [Required(ErrorMessage = "Nome do produto não informado!")]
+        [DisplayName("Nome do produto")]        
+        public string NomeProduto { get; set; }
+
+        [Required(ErrorMessage = "Valor do produto não informado!")]
+        [DisplayName("Valor do produto")]
+        public double ValorVenda { get; set; }
+
+        [Required(ErrorMessage = "Selecione a data de entrega desejada!")]
+        [DisplayName("Data de entrega desejada")]
+        public DateTime DataEntrega { get; set; }
+
+        [Required(ErrorMessage = "Forma de pagamento não informada!")]
+        [DisplayName("Forma de pagamento")]
+        [Range(1, 4, ErrorMessage ="Selecione uma forma de pagamento válida!")]
+        public TipoPagamento TipoPagamento { get; set; }
+
+        [Required(ErrorMessage = "Nome do cliente não informado!")]
+        [DisplayName("Nome do cliente")]
+        public string NomeCliente { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
