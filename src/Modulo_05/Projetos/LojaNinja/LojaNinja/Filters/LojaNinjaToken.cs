79c8b53cd504da0995848ef94b847f3c492b1dc6
Victor Comette Ribeiro <victor.ribeiro@cwinet.local>
2016-05-16 17:33:43 -0300

    feat(Aula06)

---
+++ src/Modulo_05/Projetos/LojaNinja/LojaNinja/Filters/LojaNinjaToken.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LojaNinja.MVC.Filters
+{
+    public class LojaNinjaToken : AuthorizeAttribute
+    {
+        private string[] _permissoesRequeridas = null;
+
+        public LojaNinjaToken()
+        {
+            _permissoesRequeridas = String.IsNullOrWhiteSpace(this.Roles) ? null : this.Roles.Split(',');
+        }
+    }
+}
